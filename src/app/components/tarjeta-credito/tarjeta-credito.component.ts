16f65fd4122895c2daeaa36638aa49028a51ee1b
mnbenavides <mnbenavides1@gmail.com>
2024-02-14 20:16:10 -0500

    Listado de tarjetas
    Se agrega agrega una tabla en el componete tarjeta-credito para visualizar los datos y se agrega el link de fonawesome para la visualizacion de iconos

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tarjeta-credito',
   templateUrl: './tarjeta-credito.component.html',
   styleUrls: ['./tarjeta-credito.component.css']
 })
-export class TarjetaCreditoComponent {
+export class TarjetaCreditoComponent implements OnInit {
+  public tarjetas: any[] = [
+    {nombre: 'Juan Perez', numero_tarjeta:'1546512', fecha_expiracion: '11/12', cvv:'123' },
+    {nombre: 'Maria Benavides', numero_tarjeta:'15465157', fecha_expiracion: '09/12', cvv:'789' }
+  ];
+  
+  ngOnInit():void{
+
+  }
 
 }
